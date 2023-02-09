3431dd735a604334cc5c1e71ef72266ebd8930ad
Arthur-Olivier Fortin <aofor@ulaval.ca>
2023-02-08 22:37:00 -0500

    Exercice2

---
+++ Exercice2/main2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+void main(){
+
+    char phr[20];
+    int stop = 0, i=0, maj=0, min=0;
+
+    printf("entrez une chaine de caratère en la finissant avec # \n");
+    scanf("%s", phr);
+
+    while (stop != 1){
+        if (phr[i] == ('#')){
+            stop =1;
+
+        }
+        if(phr[i] != '#'){
+            if(isupper(phr[i])){
+                maj +=1;
+                printf("xxx \n");
+            }
+            
+            else{
+                min +=1;
+
+            }
+        }
+        printf("yyy\n");
+
+        
+        printf("www\n");
+        i+=1;
+    }
+    printf("%s%d%s%d%s", "maj = ",maj, " min = ", min, "\n");
+    return;
+}
