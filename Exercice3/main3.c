dbba5fbd51d609ae08ff5ce9e438bdb3a4a99e9a
Arthur-Olivier Fortin <aofor@ulaval.ca>
2023-02-09 09:13:13 -0500

    Exercice3

--- Exercice3/main3.c
+++ Exercice3/main3.c
@@ -3,26 +3,30 @@
 
 void main(){
 
-
-    printf("Combien de chiffres voulez-vous entrer?");
+    int n, i, j;
+    printf("Combien de chiffres voulez-vous entrer?\n");
     scanf("%d", &n);
     int str[n], min, max;
 
 
-    for (i=0; i<(n+1)){
-        printf("%s%d%s", "Entrez votre chiffre", n, "\n");
-        scanf("d", str[i]);
+    for (i=0; i<(n);i++){
+        printf("%s%d%s", "Entrez votre chiffre no. ", i, "\n");
+        scanf("%d", &str[i]);
+
 
     }
 
-    for (j=0; j<(n+1)){
+    for (j=0; j<(n);j++){
+        if (j == 0){
+            min = str[j];
+        }
         if (str[j]<min){
-            min = str[j]
+            min = str[j];
         }
-        if (str[j>max]){
-            max = str[j]
+        if (str[j]>max){
+            max = str[j];
         }
     }
-    print("%s%d%s%d%s", "min = ", min, "max = ", max, "\n");
+    printf("%s%d%s%d%s", "min = ", min, " max = ", max, "\n");
     return;
 }
