231a44385523b4b71db7e6c7b869b4aa405b25cc
Arthur-Olivier Fortin <aofor@ulaval.ca>
2023-02-09 19:03:10 -0500

    Exercice8 Question3

--- Exercice8/Question3/main13.c
+++ Exercice8/Question3/main13.c
@@ -3,13 +3,13 @@
 
 void main(){
 
-    char nom;
+    char nom[20];
     int pos;
     
 
 
     printf("Entrez le nom du joueurs\n");
-    scanf("%s", &nom);
+    scanf("%s", nom);
 
     printf("Entrez la position du joueur\n");
     scanf("%d", &pos);
@@ -18,15 +18,16 @@ void main(){
 
         case 1:
             printf("%s%s", nom, " a reçu la médaille d'or\n");
-
+            break;
         case 2:
             printf("%s%s", nom, " a reçu la médaille d'argent\n");
-
+            break;
         case 3:
             printf("%s%s", nom, " a reçu la médaille de bronze\n");
-
+            break;
         default:
             printf("%s%s", nom, " n'a pas reçu de médaille");
+            break;
     }
     return;
 }
